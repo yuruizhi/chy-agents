f04961e0ab73c76272257d57efe03ba392f38605
于瑞智 <282373647@qq.com>
2025-03-11 18:09:55 +0800

    init.

---
+++ src/test/java/com/yuruizhi/ai_agent/AiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuruizhi.ai_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
